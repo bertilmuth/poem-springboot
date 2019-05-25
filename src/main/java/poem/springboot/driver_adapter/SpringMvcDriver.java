c36313a34aa0bfcc88a3435df1b38a0d7a6d0c5e
Bertil Muth <bertilmuth@yahoo.de>
2019-05-26 00:05:38 +0200

    Refactor & insert comments

--- src/main/java/poem/springboot/driver_adapter/SpringMvcDriver.java
+++ src/main/java/poem/springboot/driver_adapter/SpringMvcDriver.java
@@ -7,15 +7,19 @@ import poem.springboot.driven_adapter.SpringMvcPublisher;
 
 public class SpringMvcDriver{
 	private IReactToCommands driverPort;
-	private SpringMvcPublisher webPublisher;
+	private SpringMvcPublisher springMvcPublisher;
 
-	public SpringMvcDriver(IReactToCommands driverPort, SpringMvcPublisher webPublisher) {
+	public SpringMvcDriver(IReactToCommands driverPort, SpringMvcPublisher springMvcPublisher) {
 		this.driverPort = driverPort;
-		this.webPublisher = webPublisher;
+		this.springMvcPublisher = springMvcPublisher;
 	}
 
 	public void reactTo(Object command, Model webModel) {
-		webPublisher.setWebModel(webModel);
+		// Inform the web publisher about the current Spring MVC model,
+		// so that it can add attributes to it later.
+		springMvcPublisher.setWebModel(webModel);
+		
+		// Forward the command to the hexagon driver port.
 		driverPort.reactTo(command);
 	}
 }
